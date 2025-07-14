6297adb1c8be93633182e248702ce3f89fe448af
Hano Varghese <hanojoby@gmail.com>
2025-07-14 21:53:26 +0530

    completed

--- javascript.js
+++ javascript.js
@@ -1,26 +1,41 @@
 const container = document.querySelector('.container');
-const size = 600;
-container.style.width = `${size}px`
 
-for(let i=0;i<16*16;i++){
+function createGrid(n) {
+    container.textContent = '';
 
-    const box = document.createElement('div');
-    box.style.width = `${size/16}px`;
-    box.style.height = `${size/16}px`;
-    box.classList.add('box');
-    container.appendChild(box);
+    const size = 600;
+    container.style.width = `${size}px`;
+
+    for(let i=0;i<n*n;i++){
+
+        const box = document.createElement('div');
+        box.style.width = `${size/n}px`;
+        box.style.height = `${size/n}px`;
+        box.classList.add('box');
+        box.addEventListener('mouseenter', () => {
+            box.classList.add('hover');
+        })
+        container.appendChild(box);
+    }
 }
-
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-    box.addEventListener('mouseenter', () => {
-        box.classList.add('hover');
-    })
-})
 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', () => {
+    const boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
         box.classList.remove('hover');
     })
+})
+
+const input = document.querySelector('#grid-size');
+const setButton = document.querySelector('#grid-resize');
+setButton.addEventListener('click', () => {
+    const gridSize = parseInt(input.value);
+
+    if(gridSize < 1 || gridSize > 100){
+        alert("Please enter value between 1 and 100")
+        return;
+    }
+
+    createGrid(gridSize);
 })
